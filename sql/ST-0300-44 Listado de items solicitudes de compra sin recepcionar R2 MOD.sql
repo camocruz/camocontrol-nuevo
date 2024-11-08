874110e0e8611892c92d9795087ea3a09a740c29
carlos mosquera <camocruz@hotmail.com>
2024-11-07 21:21:18 -0500

    Se modifica la consulta ST-0300-44 para que se identifiquen los items sin recibir pero referenciando la informacion de la OC.

---
+++ sql/ST-0300-44 Listado de items solicitudes de compra sin recepcionar R2 MOD.sql
@@ -0,0 +1,62 @@
+select f0305_id_item_solicitud as id_sc_item, 
+       f0305_id_solicitud_compra as id_sc,
+       f0300_descripcion_item || ' ' || f0300_referencia as descripcion,
+       f0302_descripcion_tipo_item as tipo_item,
+       f0300_id_item as id_item,
+       f0305_ampliacion_item as descripcion_comp,
+       f0305_anotacion_item as observacion,
+       f0305_cantidad as cantidad,
+       f0002_sigla_unidad_medicion as unid,
+       to_char(f0305_fr, 'YYYY-MM-DD HH12:MI AM') as fecha_solicitud,
+       to_char(f0305_fecha_requerido, 'YYYY-MM-DD') as fecha_requiere,
+       coalesce(tb_aprobado.f0200_apellido1 || ' ' || substring(tb_aprobado.f0200_nombres from 1 for 8), 'SIN APROBAR') as aprobo,
+       to_char(f0319_fecha_aprobacion, 'YYYY-MM-DD HH12:MI AM') as f_aprobacion,
+       coalesce(f0305_id_oc, 0) as id_oc,
+       case coalesce(f0305_id_factura_compras, 0)
+          when 0 then trim( both ' ' from coalesce(tb_tercero_oc.f0200_apellido1 || ' ' || tb_tercero_oc.f0200_nombres, 'ND'))
+          else
+          trim( both ' ' from coalesce(tb_tercero_factura.f0200_apellido1 || ' ' || tb_tercero_factura.f0200_nombres, 'ND'))
+       end as proveedor, 
+       f0305_id_accion as id_acc, 
+       coalesce(f0600_id_accion_principal, coalesce(f0305_id_accion, 0)) as id_acc_ppal,
+       CASE coalesce(tb0100_estructura_mantenimiento.f0100_id_maquina_padre, 0) 
+          WHEN 0 THEN tb0100_estructura_mantenimiento.f0100_nombre
+          WHEN tb0100_estructura_mantenimiento.f0100_id_estructura THEN tb0100_estructura_mantenimiento.f0100_nombre
+          else
+          otb_maquina.f0100_nombre || ' =>> ' || tb0100_estructura_mantenimiento.f0100_nombre
+       end as nombre_estructura, 
+       CASE coalesce(tb0100_estructura_mantenimiento.f0100_id_maquina_padre, 0) 
+          WHEN 0 THEN tb0100_estructura_mantenimiento.f0100_nombre
+          WHEN tb0100_estructura_mantenimiento.f0100_id_estructura THEN tb0100_estructura_mantenimiento.f0100_nombre
+          else
+          otb_maquina.f0100_nombre
+       end as maquina
+from $df001$.tb0305_items_solicitados
+   left join $df001$.tb0307_facturas_compras
+      on f0307_id_factura_compras = f0305_id_factura_compras
+   left join $df001$.tb0200_terceros as tb_tercero_factura
+      on f0307_id_tercero = tb_tercero_factura.f0200_id_tercero 
+   join $df001$.tb0304_solicitud_compra
+      on f0305_id_solicitud_compra = f0304_id_solicitud
+   join $df001$.tb0300_items
+      on f0305_id_item = f0300_id_item
+   join $df001$.tb0002_unidades_medicion
+      on f0300_id_unidad_medicion = f0002_id_unidad_medicion
+   join $df001$.tb0302_tipos_items
+      on f0302_id_tipo_item = f0300_id_tipo_item
+   left join $df001$.tb0600_acciones
+      on f0600_id_accion = f0305_id_accion
+   left join $df001$.tb0100_estructura_mantenimiento
+      on f0305_id_estructura = f0100_id_estructura
+   left join $df001$.tb0100_estructura_mantenimiento as otb_maquina
+      on otb_maquina.f0100_id_estructura = tb0100_estructura_mantenimiento.f0100_id_maquina_padre
+   left join $df001$.tb0319_ordenes_compra
+   	  on f0319_id_oc = f0305_id_oc
+   left join $df001$.tb0200_terceros as tb_tercero_oc
+      on f0319_id_tercero = tb_tercero_oc.f0200_id_tercero 
+   left join $df001$.tb0200_terceros as tb_aprobado
+      on f0319_usuario_aprobar = tb_aprobado.f0200_id_tercero
+Where f0305_anulado = 'N' and f0305_id_cia = '$001$'
+   and f0305_fr BETWEEN '$002$' and '$003$'
+   and f0305_id_factura_compras is null 
+order by f0300_descripcion_item, f0305_id_solicitud_compra
