b75b3d4f915a1954bf7bf97553c02da3829465aa
Carlos Mosquera <camocruz@hotmail.com>
2025-12-29 22:32:46 -0500

    copia funcional corrigiendo problema para conectar con la BD

--- camocontrol/My Project/Application.Designer.vb
+++ camocontrol/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.camocontrol.fm_0800_cargar_pedidos_unoee
+            Me.MainForm = Global.camocontrol.login
         End Sub
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
